cc6cb7cd00675f3d51030c6d3ee101465e5da845
jonascicek <jonascicek98@gmail.com>
2025-01-14 12:43:58 +0100

    postcss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        metalmania: ['Metalmania', ...defaultTheme.fontFamily.sans],
+        metalmania: ['Metalmania'],
       }
     },
   },
